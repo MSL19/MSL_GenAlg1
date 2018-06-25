d1a442b83cb7325ca8966fff2a0dfd796348097c
MSL19 <40368820+MSL19@users.noreply.github.com>
2018-06-24 23:08:07 -0600

    bad news
    asdascvsdfasvsavwd

--- cashRegister.cpp
+++ cashRegister.cpp
@@ -72,9 +72,125 @@ int main()
 
 				for (int i = 0; i, POP_SIZE; i++)
 				{
-					if()
+					if (Popultion[i].fitness == 999.0f)
+					{
+						cout << "\nSolution Found in asdsadasds " << GenerationRequiredToFindASolution << " generations!!!!" << endl << endl;;
+						PrintChromo(Population[i].bits);
+						solutionFound = true;
+						break;
+						
+					}
 				}
 				
+
+
+				chromo_typ temp[POP_SIZE];
+
+				int cPop = 0;
+
+				//loop until we have created POP_SIZE new chromosomes
+				while (cPop < POP_SIZE)
+				{
+					// we are going to create the new population by grabbing members of the old population
+					// two at a time via roulette wheel selection.
+					string offspring1 = Roulette(TotalFitness, Population);
+					string offspring2 = Roulette(TotalFitness, Population);
+
+					//add crossover dependent on the crossover rate
+					Crossover(offspring1, offspring2);
+
+					//now mutate dependent on the mutation rate
+					Mutate(offspring1);
+					Mutate(offspring2);
+
+					//add these offspring to the new population. (assigning zero as their
+					//fitness scores)
+					temp[cPop++] = chromo_typ(offspring1, 0.0f);
+					temp[cPop++] = chromo_typ(offspring2, 0.0f);
+
+				}//end loop
+
+				 //copy temp population into main population array
+				for (int i = 0; i<POP_SIZE; i++)
+				{
+					Population[i] = temp[i];
+				}
+
+				//i'm tired
+				++GenerationsRequiredToFindASolution;
+
+				// exit app if no solution found within the maximum allowable number
+				// of generations
+				if (GenerationsRequiredToFindASolution > MAX_ALLOWABLE_GENERATIONS)
+				{
+					cout << "No solutions found this run!";
+
+					bFound = true;
+				}
+
+			}
+
+			cout << "\n\n\n";
+
+		}//end while
+
+		return 0;
+	}
+
+
+
+
+	//---------------------------------GetRandomBits-----------------------------------------
+	//
+	//	This function returns a string of random 1s and 0s of the desired length.
+	//
+	//-----------------------------------------------------------------------------------------
+	string	GetRandomBits(int length)
+	{
+		string bits;
+
+		for (int i = 0; i<length; i++)
+		{
+			if (RANDOM_NUM > 0.5f)
+
+				bits += "1";
+
+			else
+
+				bits += "0";
+		}
+
+		return bits;
+	}
+
+	//---------------------------------BinToDec-----------------------------------------
+	//
+	//	converts a binary string into a decimal integer
+	//
+	//-----------------------------------------------------------------------------------
+	int	BinToDec(string bits)
+	{
+		int val = 0;
+		int value_to_add = 1;
+
+		for (int i = bits.length(); i > 0; i--)
+		{
+
+
+			if (bits.at(i - 1) == '1')
+
+				val += value_to_add;
+
+			value_to_add *= 2;
+
+		}//next bit
+
+		return val;
+	}
+
+
+
+
 			}
 
 		}
